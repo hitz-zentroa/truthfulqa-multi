327b01bfa49b1299af42e3d7f0e6c264a4a0ae80
Julen Etxaniz <juletxara@gmail.com>
2024-07-05 12:06:21 +0200

    add expriments scripts

---
+++ experiments/experiment.sh
@@ -0,0 +1,15 @@
+
+AUTHOR=$1
+MODEL=$2
+
+for lang in en ca es eu gl
+    do
+    lm_eval --model hf \
+        --model_args pretrained=$AUTHOR/$MODEL \
+        --tasks truthfulqa-multi_gen_$lang \
+        --batch_size auto \
+        --log_samples \
+        --device cuda \
+        --output_path results/output/$lang/results_$MODEL.json 
+
+    done
