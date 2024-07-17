40aa4d847893c7cfeb68b4ed4b9c5d2051986e59
Blanca <blancacalvofigueras@gmail.com>
2024-07-17 16:00:19 +0200

    eager attn for gemma

---
+++ experiments/run/experiment_eager.sh
@@ -0,0 +1,18 @@
+
+AUTHOR=$1
+MODEL=$2
+
+for lang in ca es eu gl en 
+    do
+    lm_eval --model hf \
+        --model_args pretrained=$AUTHOR/$MODEL,attn_implementation=eager \
+        --tasks truthfulqa-multi_gen_$lang \
+        --batch_size auto \
+        --log_samples \
+        --device cuda \
+        --output_path results/gen/$lang \
+        --apply_chat_template \
+        --fewshot_as_multiturn \
+        --num_fewshot 6
+
+    done
