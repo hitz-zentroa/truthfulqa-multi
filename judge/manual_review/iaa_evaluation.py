edadd38fbf8c0e4a9e96691e5edaabff7548e44a
Blanca <blancacalvofigueras@gmail.com>
2024-07-30 11:04:26 +0200

    iaa of manual evaluation

--- judge/manual_review/iaa_evaluation.py
+++ judge/manual_review/iaa_evaluation.py
@@ -1,15 +1,47 @@
 import json
+import csv
+from sklearn.metrics import cohen_kappa_score
+from collections import Counter
 
 def main():
-    print()
+    langs = ['en', 'es', 'ca', 'eu', 'gl']
 
     # TODO: for each language
+    for lang in langs:
+        # load the results evaluated/lang
+        with open('judge/manual_review/evaluated/'+lang+'.csv') as f:
+            r = csv.reader(f)
+            next(r)
+            data = []
+            for line in r:
+                data.append(line)
 
-    # load the results evaluated/lang
+        #load iaa/lang
+        with open('judge/manual_review/iaa/'+lang+'.csv') as f:
+            r = csv.reader(f)
+            next(r)
+            iaa = []
+            for line in r:
+                iaa.append(line)
 
-    #load iaa/lang
+        # find same instances and calculate iaa
+        truth_labels = []
+        iaa_truth_labels = []
+        info_labels = []
+        iaa_info_labels = []
+        for line in iaa:
+            iaa_truth_labels.append(line[5])
+            iaa_info_labels.append(line[6])
+            for entry in data:
+                if entry[0] == line [0]:
+                    truth_labels.append(entry[5])
+                    info_labels.append(entry[6])
+                    break
 
-    # find same instances and calculate iaa
+
+        print(len(truth_labels), len(info_labels), len(iaa_info_labels), len(iaa_truth_labels))
+        print(cohen_kappa_score(truth_labels, iaa_truth_labels))
+        print(cohen_kappa_score(info_labels, iaa_info_labels))
 
     
 
