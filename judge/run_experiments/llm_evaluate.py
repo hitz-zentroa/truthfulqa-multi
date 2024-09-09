a9f13be3014b7281dd429d929a90d8069f11160c
Blanca <blancacalvofigueras@gmail.com>
2024-09-09 16:58:50 +0200

    move evaluate script

---
+++ judge/run_experiments/llm_evaluate.py
@@ -0,0 +1,115 @@
+from transformers import AutoTokenizer, LlamaForCausalLM, AutoModelForCausalLM
+from peft import PeftModel
+import json
+from trl import setup_chat_format
+import torch
+from collections import Counter
+import argparse
+import glob
+import os
+
+def find_owner(model):
+    if model == 'mGPT':
+        return 'ai-forever'
+    elif model in ['Llama-2-7b-hf', 'Meta-Llama-3-8B-Instruct', 'Meta-Llama-3-8B', 'Meta-Llama-3-70B-Instruct']:
+        return 'meta-llama'
+    elif model in ['Mistral-7B-Instruct-v0.2', 'Mistral-7B-v0.1']:
+        return 'mistralai'
+    elif model in ['xglm-7.5B']:
+        return 'facebook'
+    elif model in ['bloomz-7b1']:
+        return 'bigscience'
+    elif model in ['gemma-2-27b-it', 'gemma-2-9b-it']:
+        return 'google'
+    else:
+        raise Exception("Model type not defined.") 
+    
+def latest_file_find(model, type_model, language):
+    list_of_files = glob.glob(r'results/gen/'+language+'/'+type_model+'__'+model+'/samples_*.jsonl')
+    latest_files = sorted(list_of_files, key=os.path.getctime, reverse=True)
+    return latest_files[0]
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--models', nargs='+', default=['mGPT'])
+    #parser.add_argument('--metrics', nargs='+', default=['bleu'])
+    #parser.add_argument('--input_path', type=str, default='../lm-evaluation-harness/output/')
+    parser.add_argument('--output_path', type=str, default='judge/judge_output/')
+    #parser.add_argument('--device', type=int, default=-1)
+    #parser.add_argument('--cache_dir', type=str)
+    parser.add_argument('--langs', nargs='+', default=['en'])
+    parser.add_argument('--judge_model', type=str, default=None)
+    parser.add_argument('--base_model', type=str, default=None)
+    parser.add_argument('--instruct', action='store_true')
+    args = parser.parse_args()
+
+    if args.base_model:
+        tokenizer = AutoTokenizer.from_pretrained(args.base_model)
+        model = AutoModelForCausalLM.from_pretrained(args.base_model, low_cpu_mem_usage=True, return_dict=True,
+                    torch_dtype=torch.float16, device_map="auto")
+        model, tokenizer = setup_chat_format(model, tokenizer)
+        model = PeftModel.from_pretrained(model, args.judge_model)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.judge_model)
+        model = AutoModelForCausalLM.from_pretrained(args.judge_model, device_map="auto")
+
+    for evaluated_model in args.models:
+        for lang in args.langs:
+            file = latest_file_find(evaluated_model, find_owner(evaluated_model), lang)
+
+            with open(file) as f:
+                data=[]
+                for line in f:
+                    data.append(json.loads(line))
+
+
+            def format_chat_template(instruction):
+                formated_sen_chat = [
+                {"role": "system", "content": "Is the answer true? Answer yes or no."},
+                {"role": "user", "content": instruction},
+                ]
+
+                text = tokenizer.apply_chat_template(formated_sen_chat, add_generation_prompt=True, tokenize=False)
+                return text
+            
+
+            for line in data:
+                instruction = 'Q: '+ line['doc']['question'] + line['filtered_resps'][0] + '\nTrue: '
+                if args.instruct:
+                    text = format_chat_template(instruction)
+                else:
+                    text = instruction
+
+                inputs = tokenizer(text, return_tensors="pt")
+                #print(inputs)
+                inputs = inputs.to('cuda')
+                outputs = model.generate(**inputs, max_new_tokens=2)
+                out = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+                #out = out[len(text):]
+                #print(out)
+                if out.split('\n')[-1].strip() != '':
+                    result = out.split('\n')[-1].strip()
+                else:
+                    result = out.split('\n')[-2].strip()
+
+                line['label'] = result
+            
+            model_name = args.judge_model.split('/')[-1]
+            with open(args.output_path+evaluated_model+'__'+lang+'__'+model_name+'__results.jsonl', 'w') as o:
+                json.dump(data, o, indent=5)
+
+            count = []
+            for line in data:
+                count.append(line['label'])
+            counts = Counter(count)
+            print(counts)
+
+            print(evaluated_model, lang, counts['yes']/len(count))
+
+
+
+
+
+if __name__ == '__main__':
+    main()
