d4d848038fcf0267049a67700f7489767b59c332
Blanca <blancacalvofigueras@gmail.com>
2024-11-25 11:07:02 +0100

    MT experiments

---
+++ MT_experiments/run/experiment_eager.sh
@@ -0,0 +1,18 @@
+
+AUTHOR=$1
+MODEL=$2
+
+for lang in ca es eu gl
+    do
+    lm_eval --model hf \
+        --model_args pretrained=$AUTHOR/$MODEL,attn_implementation=eager \
+        --tasks truthfulqa-multi-MT_gen_$lang \
+        --batch_size auto \
+        --log_samples \
+        --device cuda \
+        --output_path results-MT/gen/$lang \
+        --apply_chat_template \
+        --fewshot_as_multiturn \
+        --num_fewshot 6
+
+    done
---
+++ MT_experiments/run/experiment_eager_no_instruct.sh
@@ -0,0 +1,16 @@
+
+AUTHOR=$1
+MODEL=$2
+
+for lang in ca es eu gl
+    do
+    lm_eval --model hf \
+        --model_args pretrained=$AUTHOR/$MODEL,attn_implementation=eager \
+        --tasks truthfulqa-multi-MT_gen_$lang \
+        --batch_size auto \
+        --log_samples \
+        --device cuda \
+        --output_path results-MT/gen/$lang \
+        --num_fewshot 6
+
+    done
